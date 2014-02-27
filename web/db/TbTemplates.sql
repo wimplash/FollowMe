f7e9b0ae46de7081cfa8eca76dcf1cd6ed4d5cea
SteveDaug <steve.l.daugherty@gmail.com>
2014-02-27 17:28:40 -0500

    DB Tables

--- web/db/TbTemplates.sql
+++ web/db/TbTemplates.sql
@@ -1,7 +1,7 @@
 USE [FollowMe]
 GO
 
-/****** Object:  Table [dbo].[TbTemplates]    Script Date: 02/27/2014 17:12:15 ******/
+/****** Object:  Table [dbo].[TbTemplates]    Script Date: 02/27/2014 17:27:41 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,7 +12,6 @@ CREATE TABLE [dbo].[TbTemplates](
 	[tID] [int] NOT NULL,
 	[tName] [nvarchar](50) NULL,
 	[tDesc] [nvarchar](200) NULL,
-	[tPOI] [nvarchar](4000) NULL,
  CONSTRAINT [PK_TbTemplates] PRIMARY KEY CLUSTERED 
 (
 	[tID] ASC
