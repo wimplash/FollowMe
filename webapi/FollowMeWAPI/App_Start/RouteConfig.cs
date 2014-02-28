b4077ac0d91d53eb27969231c83e97520abe0b6f
wimplash <rdransfo@gmail.com>
2014-02-28 12:13:00 -0500

    adding IgnoeRoute for static files

--- webapi/FollowMeWAPI/App_Start/RouteConfig.cs
+++ webapi/FollowMeWAPI/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace FollowMeWAPI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.IgnoreRoute("{*staticfiles}", new { staticfiles = @".*\.(css|js|gif|png|jpg)(/.*)?" })
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
